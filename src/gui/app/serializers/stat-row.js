9e828043472333aaa1a20fb0c3741952e0d82b77
Mark <mark.hall@work.room3b.eu>
2015-11-07 15:20:38 +0000

    Upgrade to Ember 13.8

--- src/gui/app/serializers/stat-row.js
+++ src/gui/app/serializers/stat-row.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
     attrs: {
         columns: {embedded: 'always'}
-    }
+    },
+    isNewSerializerAPI: true
 });
--- src/gui/app/serializers/stat-table.js
+++ src/gui/app/serializers/stat-table.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
     attrs: {
         rows: {embedded: 'always'}
-    }
+    },
+    isNewSerializerAPI: true
 });
