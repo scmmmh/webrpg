886a35de09eefa5da7f240997c0b6c6189e7d745
Mark <mark.hall@work.room3b.eu>
2016-08-02 18:08:23 +0100

    Simplified template structure through plugin.

--- src/gui/app/models/game-role.js
+++ src/gui/app/models/game-role.js
@@ -6,14 +6,4 @@ export default DS.Model.extend({
     is_me: DS.attr('boolean', {defaultValue: false}),
     user: DS.belongsTo('User'),
     game: DS.belongsTo('Game'),
-    
-    is_not_me: Ember.computed('is_me', function() {
-        return !this.get('is_me');
-    }),
-    is_owner: Ember.computed('role', function() {
-        return this.get('role') === 'owner';
-    }),
-    is_player: Ember.computed('role', function() {
-        return this.get('role') === 'player';
-    })
 });
