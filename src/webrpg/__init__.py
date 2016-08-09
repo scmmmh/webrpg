c9af3242578ab910114daf23aa48a7b3bbfd01d9
Mark <mark.hall@work.room3b.eu>
2016-08-09 17:54:03 +0100

    Transition to Python3.

--- src/webrpg/__init__.py
+++ src/webrpg/__init__.py
@@ -12,7 +12,6 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     views.init(config)
     config.scan()
