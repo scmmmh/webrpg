3565d59735500009b1f214ec71087c352c122c7e
Mark <mark.hall@work.room3b.eu>
2016-08-10 20:40:14 +0100

    Fix the accordion.

--- src/gui/app/components/fd-accordion.js
+++ src/gui/app/components/fd-accordion.js
@@ -3,11 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     tagName: 'ul',
     classNames: ['accordion'],
-    attributeBindings: ['data-accordion', 'data-options'],
-    'data-accordion': '',
-    'data-options': '',
-    
-    didInsertElement: function() {
-        //this.$().foundation();
+
+    click: function(ev) {
+        var link = $(ev.target);
+        console.log(link);
+        if(link.hasClass('accordion-title')) {
+            ev.preventDefault();
+            if(link.parent().hasClass('is-active')) {
+                this.get('selectTab')('');
+                link.parent().removeClass('is-active');
+            } else {
+                this.get('selectTab')(link.parent().data('accordion-item'));
+                link.parent().addClass('is-active');
+            }
+        }
     }
 });
