8d05260fe6b154472adce6a0c7afbf68b9a9bf04
Mark <Mark.Hall@work.room3b.eu>
2016-08-14 14:56:28 +0100

    Split source message and formatted message in the ChatMessage to reduce the security exposure.

---
+++ src/gui/tests/unit/helpers/format-chat-message-test.js
@@ -0,0 +1,10 @@
+import { formatChatMessage } from 'gui/helpers/format-chat-message';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format chat message');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatChatMessage([42]);
+  assert.ok(result);
+});
--- src/webrpg/scripts/populate.py
+++ src/webrpg/scripts/populate.py
@@ -7,10 +7,11 @@ Implements the database initialisation functionality.
 
 .. moduleauthor:: Mark Hall <mark.hall@work.room3b.eu>
 """
+from alembic import command, config
 from pyramid.paster import (get_appsettings, setup_logging)
 from sqlalchemy import engine_from_config
 
-from webrpg.models import (Base)
+from webrpg.models import (Base, DB_VERSION)
 
 
 def init(subparsers):
@@ -19,6 +20,12 @@ def init(subparsers):
     parser.add_argument('configuration', help='WebRPG configuration file')
     parser.add_argument('--drop-existing', action='store_true', default=False, help='Drop any existing tables')
     parser.set_defaults(func=initialise_database)
+    parser = subparsers.add_parser('update-database', help='Update the Web Teaching Environment database')
+    parser.add_argument('configuration', help='Configuration file')
+    parser.set_defaults(func=update_database)
+    parser = subparsers.add_parser('downgrade-database', help='Downgrade the Web Teaching Environment database')
+    parser.add_argument('configuration', help='Configuration file')
+    parser.set_defaults(func=downgrade_database)
 
 
 def initialise_database(args):
@@ -30,3 +37,18 @@ def initialise_database(args):
     if args.drop_existing:
         Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+    alembic_config = config.Config(args.configuration, ini_section='app:main')
+    alembic_config.set_section_option('app:main', 'script_location', 'webrpg:migrations')
+    command.stamp(alembic_config, "head")
+
+
+def update_database(args):
+    alembic_config = config.Config(args.configuration, ini_section='app:main')
+    alembic_config.set_section_option('app:main', 'script_location', 'webrpg:migrations')
+    command.upgrade(alembic_config, DB_VERSION)
+
+
+def downgrade_database(args):
+    alembic_config = config.Config(args.configuration, ini_section='app:main')
+    alembic_config.set_section_option('app:main', 'script_location', 'webrpg:migrations')
+    command.downgrade(alembic_config, '-1')
